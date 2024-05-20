4fe28e321a438d4a172424dbf76561c0bedd700e
bolemir1927 <maksim_petrov_88@mail.ru>
2024-05-20 23:47:23 +0300

    20052024

--- src/main/java/hardtask/HardFive.java
+++ src/main/java/hardtask/HardFive.java
@@ -1,8 +1,18 @@
 package hardtask;
 
+import java.util.Scanner;
+
 public class HardFive {
     public static void main(String[] args) {
         //Проверка года на високосность или наличие 29 февраля: Напишите программу, которая запрашивает год
         //у пользователя и проверяет, является ли он високосным или содержит ли в себе 29 февраля.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите год: ");
+        int number = sc.nextInt();
+        if (number%4 == 0  && number%100 != 0 || number%400 == 0){
+            System.out.println(number + " високосный");
+        }else {
+            System.out.println(number + " невисокосный");
+        }
     }
 }
--- src/main/java/hardtask/HardFour.java
+++ src/main/java/hardtask/HardFour.java
@@ -6,16 +6,18 @@ public class HardFour {
     public static void main(String[] args) {
         //Проверка логина и пароля: Напишите программу, которая запрашивает у пользователя логин и пароль и проверяет их.
         // Пользователь будет авторизован, если логин равен "admin" и пароль равен "password".
+        String login = "admin";
+        String pas = "password";
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите логин: ");
-        String login = sc.next();
-        if (login == "admin") {
+        String admin = sc.next();
+        if (login.equals(admin)) {
             System.out.println("Введите пароль: ");
         } else {
             System.out.println("Логин не верный");
         }
-        String pas = sc.next();
-        if (pas == "password") {
+        String password = sc.next();
+        if (pas.equals(password)) {
             System.out.println("Accept");
         } else {
             System.out.println("Error");
