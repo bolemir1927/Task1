0b6a21da4c3b68b82fabcb138398d1527fe49e75
bolemir1927 <maksim_petrov_88@mail.ru>
2024-05-21 23:52:49 +0300

    21052024

--- src/main/java/hardtask/HardTen.java
+++ src/main/java/hardtask/HardTen.java
@@ -8,23 +8,19 @@ public class HardTen {
         //Человек может получить займ, если его возраст больше 18 лет (age > 18), его кредитный рейтинг
         //выше 700 (creditScore > 700) и он имеет стабильный источник дохода (hasStableIncome).
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите возраст: ");
+        {
+            System.out.println("Введите возраст: ");
+        }
         int age = sc.nextInt();
-        if (age > 18) {
-            System.out.println("Подтверждено ");
-        } else {
-            System.out.println("Отказано");
+        {
+            System.out.println("Введите кредитный рейтинг: ");
         }
-        System.out.println("Введите кредитный рейтинг: ");
-        int credit = sc.nextInt();
-        if (credit > 700) {
-            System.out.println("Подтверждено ");
-        } else {
-            System.out.println("Отказано ");
+        int creditScore = sc.nextInt();
+        {
+            System.out.println("Введите источник дохода: ");
         }
-        System.out.println("Введите источник дохода: ");
         String income = sc.next();
-        if (income.equals("hasStableIncome")) {
+        if (age > 18 && creditScore > 700 && income.equals("hasStableIncome")) {
             System.out.println("Accept");
         } else {
             System.out.println("Error");
